d030b5efcc69a38e795d358b82bc238a6c3a1455
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-06-25 07:44:12 +0200

    sexp

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,7 @@
-let () = Printf.printf "%s\n" Hello.Es.v
-let () = Printf.printf "%s\n" Hello.En.v
+(* Read in Sexp from string *)
+let exp1 = Sexplib.Sexp.of_string "(This (is an) (s expression))"
+
+(* Do something with the Sexp ... *)
+
+(* Convert back to a string to print *)
+let () = Printf.printf "%s\n" (Sexplib.Sexp.to_string exp1)
